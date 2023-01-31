4a7c9750101023403e2fde6c163b7a8256a0a154
shuangxuetwelve <shuangxue12@outlook.com>
2023-01-30 22:01:31 -0500

    Can make predictions

--- torchserve/handler.py
+++ torchserve/handler.py
@@ -1,5 +1,24 @@
+# Third-party imports
 import ts.torch_handler.vision_handler
+import ts.utils.util
+import torch
+import torchvision
 
 class Handler(ts.torch_handler.vision_handler.VisionHandler):
 
-    ktop = 5
+    # Constants
+    image_scale_size = 576
+    topk = 5
+
+    image_processing = torchvision.transforms.Compose([
+        torchvision.transforms.Resize((image_scale_size, image_scale_size)),
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
+    ])
+
+    def postprocess(self, data):
+        confidences = torch.sigmoid(data)
+        probs, classes = torch.topk(confidences, self.topk, dim=1)
+        print(probs) # tensor([[0.9995, 0.9862, 0.9412, 0.6772, 0.0021]], device='cuda:0')
+        print(classes) # tensor([[14,  4, 10,  8, 13]], device='cuda:0')
+        return ts.utils.util.map_class_to_label(probs.tolist(), self.mapping, classes.tolist())
--- torchserve/model/model.py
+++ torchserve/model/model.py
@@ -1,10 +1,20 @@
+# Third-party imports.
+import torch
+
+# Self imports.
 from CTran import CTranModel
 
 class Model(CTranModel):
     
-    num_labels = 1000
+    num_labels = 20
     use_lmt = True
     dropout = 0
 
     def __init__(self):
         super(Model, self).__init__(num_labels=self.num_labels, use_lmt=self.use_lmt, dropout=self.dropout)
+
+    def forward(self, images):
+        mask = torch.full((self.num_labels,), -1)
+        masks = mask[None, :]
+        predictions, _, _ = super(Model, self).forward(images, masks.cuda())
+        return predictions
